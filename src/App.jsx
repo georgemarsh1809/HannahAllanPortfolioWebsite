7dfce2d59f7b33f3df5f0e87664cba1c0fbc753e
George Marsh <georgemarsh1809@gmail.com>
2025-04-06 11:02:23 +0530

    First full commit - empty site

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='parent-container'>
+        A website...
+
+      </div>
+    </>
+  )
+}
+
+export default App
