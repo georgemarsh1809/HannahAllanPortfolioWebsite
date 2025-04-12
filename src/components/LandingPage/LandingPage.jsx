2217dac839993715a73703233d3bf1ad97ee464d
George Marsh <georgemarsh1809@gmail.com>
2025-04-12 11:45:47 +0530

    Completed imp of landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './LandingPage.module.css'
+import { Slider } from './components/Slider'
+
+import img1 from '../../assets/LandingPagePhotos/landingpagecarousel-001.jpg'
+import img2 from '../../assets/LandingPagePhotos/landingpagecarousel-002.jpg'
+import img3 from '../../assets/LandingPagePhotos/landingpagecarousel-003.jpg'
+import img4 from '../../assets/LandingPagePhotos/landingpagecarousel-004.jpg'
+import img5 from '../../assets/LandingPagePhotos/landingpagecarousel-005.jpg'
+import img6 from '../../assets/LandingPagePhotos/landingpagecarousel-006.jpg'
+import img7 from '../../assets/LandingPagePhotos/landingpagecarousel-007.jpg'
+import img8 from '../../assets/LandingPagePhotos/landingpagecarousel-008.jpg'
+import img9 from '../../assets/LandingPagePhotos/landingpagecarousel-009.jpg'
+import img10 from '../../assets/LandingPagePhotos/landingpagecarousel-010.jpg'
+import img11 from '../../assets/LandingPagePhotos/landingpagecarousel-011.jpg'
+import img12 from '../../assets/LandingPagePhotos/landingpagecarousel-012.jpg'
+import img13 from '../../assets/LandingPagePhotos/landingpagecarousel-013.jpg'
+import img14 from '../../assets/LandingPagePhotos/landingpagecarousel-014.jpg'
+// import images from '../../../../assets/LandingPagePhotos'
+
+const IMAGES = [img1, img2, img3,
+    img4, img5, img6,
+    img7, img8, img9,
+    img10, img11, img12, img13, img14]
+
 
 export const LandingPage = () => {
     return (
-        <div>
-
-        </div>
+        // <div className={styles.landingPageParent}>
+        <Slider imageUrls={IMAGES} />
     )
 }
---
+++ src/components/LandingPage/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import styles from './Slider.module.css'
+// import ImageSlider, { Slide } from 'react-auto-image-slider'
+
+
+export const Slider = ({ imageUrls }) => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, [imageUrls.length]);
+
+    if (!imageUrls || imageUrls.length === 0) {
+        return <div>No images provided.</div>;
+    }
+
+    return (
+        <div className={styles.sliderParent}>
+            <div className={styles.slider}>
+                {imageUrls.map((imageUrl, index) => (
+                    <img
+                        key={`image-${index}`}
+                        src={imageUrl}
+                        alt={`Image ${index + 1}`}
+                        className={`${styles.image} ${index === currentIndex ? styles.active : styles.inactive}`}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+
