b12ab07c8c8975288a6a27b86195442eb82daf8a
George Marsh <georgemarsh1809@gmail.com>
2025-05-05 13:34:30 +0530

    Works page reimplemented, almost complete

--- src/components/WorksPage/WorksPage.jsx
+++ src/components/WorksPage/WorksPage.jsx
@@ -5,28 +5,33 @@ import styles from './WorksPage.module.css'
 export const WorksPage = () => {
     return (
         <div className={styles.worksPageContainer}>
+            <div className={styles.worksPageRowContainer}>
 
-            <div className={styles.projectTileOne}>
-                <ProjectTile imageNumber={2} project={"InnerChildLiving"}></ProjectTile>
+                <div className={styles.projectTileOne}>
+                    <ProjectTile imageNumber={2} project={"InnerChildLiving"}></ProjectTile>
+                </div>
+                <div className={styles.projectTileTwo}>
+                    <ProjectTile imageNumber={3} project={"TenthPlanet"}></ProjectTile>
+                </div>
+            </div>
+            <div className={styles.worksPageRowContainer}>
 
+                <div className={styles.projectTileThree}>
+                    <ProjectTile imageNumber={15} project={"MonsoonFN"}></ProjectTile>
+                </div>
+                <div className={styles.projectTileFour}>
+                    <ProjectTile imageNumber={0} project={"SacredSpaces"}></ProjectTile>
+                </div>
             </div>
-            <div className={styles.projectTileTwo}>
-                <ProjectTile imageNumber={3} project={"TenthPlanet"}></ProjectTile>
-
+            <div className={styles.worksPageRowContainer}>
+                <div className={styles.projectTileFive}>
+                    <ProjectTile imageNumber={0} project={"Fractured"}></ProjectTile>
+                </div>
+                <div className={styles.projectTileSix}>
+                    <ProjectTile imageNumber={0} project={"Chris&Chamilla"}></ProjectTile>
+                </div>
             </div>
-            <div className={styles.projectTileThree}>
-                <ProjectTile imageNumber={15} project={"MonsoonFN"}></ProjectTile>
-            </div>
-            <div className={styles.projectTileFour}>
-                <ProjectTile imageNumber={0} project={"SacredSpaces"}></ProjectTile>
-            </div>
-            <div className={styles.projectTileFive}>
-                <ProjectTile imageNumber={0} project={"Fractured"}></ProjectTile>
-            </div>
-            <div className={styles.projectTileSix}>
-                <ProjectTile imageNumber={0} project={"Chris&Chamilla"}></ProjectTile>
-            </div>
-
         </div>
+
     )
 }
